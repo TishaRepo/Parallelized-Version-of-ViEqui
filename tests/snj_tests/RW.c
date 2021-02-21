3250ec700635b936a3b9d8da276c8b0d3f25d3a9
singhsanjana <sanjana.singh2906@gmail.com>
2021-02-21 19:32:34 +0530

    our tests

---
+++ tests/snj_tests/RW.c
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <pthread.h>
+
+int x; 
+
+void *t0(void *arg){
+  x = 1;
+
+  return NULL;
+}
+
+void *t1(void *arg){
+  int a = x;
+  
+  return NULL;
+}
+
+int main(int argc, char *argv[]){
+  pthread_t thr0; 
+  pthread_t thr1; 
+
+  x = 0;
+
+  pthread_create(&thr0, NULL, t0, NULL);
+  pthread_create(&thr1, NULL, t1, NULL);
+
+  pthread_join(thr0, NULL);
+  pthread_join(thr1, NULL);
+
+  return 0;
+}
