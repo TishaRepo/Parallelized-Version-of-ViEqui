360e17e2c6987d47235b366128d4306e08089dae
singhsanjana <sanjana.singh2906@gmail.com>
2022-05-29 02:36:16 +0530

    updated test script for assuming TO

--- scripts/sv-bench.py
+++ scripts/sv-bench.py
@@ -1,8 +1,8 @@
-from locale import T_FMT_AMPM
 import os
 import sys
 import subprocess
-from threading import Timer
+import argparse
+
 from time import time
 from datetime import datetime
 from output_colors import output_colors as oc
@@ -12,14 +12,27 @@ TO = 1800 # 30 mins
 executable_file = 'sv_bench_executable_file.ll'
 #--------------------------------------------------
 
+# default -----------------------------------------
+bench_path = 'benchmarks/sv_bench/'
+
+# arguments ---------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument("--dir-path", "-d", type=str, required=False, dest="bench_path",
+					help="Path to test directory.")
+parser.add_argument("--test-configurations", "-c", required=False, action='store_true', dest="has_configurations",
+					help="Tests have configurations.")
+
+args = parser.parse_args()
+bench_path  = args.bench_path
+has_configs = args.has_configurations
+
 # initial setup -----------------------------------
-if len(sys.argv) == 1:
-    bench_dir  = 'pthread'
-    bench_path = 'benchmarks/sv_bench/' + bench_dir + '/'
-else:
-    bench_path = sys.argv[1]
-    if bench_path[-1] != '/':
-        bench_path += '/'
+# if len(sys.argv) == 1:
+    
+# else:
+#     bench_path = sys.argv[1]
+#     if bench_path[-1] != '/':
+#         bench_path += '/'
 
 benchdirs = [bench_path]
 
@@ -70,6 +83,48 @@ def make_csv_row(trace_count, time, error, i):
     
     return row
 
+def generate_assumed_TO_configs(config, last_config, last_status_TO): 
+    assumed_TO_configs = []
+
+    for i in last_config: # last TO config of i.th command
+        assert(len(last_config[i]) == 0 or len(config) == len(last_config[i][0])) # assert same config format
+
+        if last_status_TO[i]: # an earlier config TOed
+            strictly_larger_test = False # larger than atleast one TOed config
+            for j in range(len(last_config[i])): # for each incomparable TO config
+                strictly_larger_test_than_current = True # new config is strictly larger than current TOed config
+                for k in range(len(last_config[i][j])): # for each parameter of last_config[i][j]
+                    if int(last_config[i][j][k]) > int(config[k]):
+                        strictly_larger_test_than_current = False
+                        break
+        
+                if strictly_larger_test_than_current:
+                    strictly_larger_test = True # new config is strictly larger a TOed config
+                    break
+                    
+            if strictly_larger_test: # new config is strictly larger a TOed config
+                assumed_TO_configs.append(i) # this config will also TO, so do not test
+    
+    return assumed_TO_configs
+
+def get_config(file):
+    config = ( file[:-2].split('_conf_')[-1] ).split('-') # file name format: bench_dir_conf_config
+    config = [int(x) for x in config]
+    return config
+
+def sort_by_config(bench_files):
+    configs = []
+    for file in bench_files:
+        configs.append(get_config(file))
+    return [ [str(x) for x in y] for y in sorted(configs) ]
+
+def record_TOs(tests_timedout, config, last_status_TO, last_config, assumed_TO_configs):
+    for i in range(len(tests_timedout)): 
+        if tests_timedout[i]: # command timedout on current config
+            if not i in assumed_TO_configs: # TO after run (not assumed)
+                last_status_TO[i] = True
+                last_config[i].append(config)
+
 def print_dir_summary(bench_dir, tests_completed, tests_failed, tests_timedout):
     header = 'Test Set sv-benchmarks::' + bench_dir
     header_length = len(header)
@@ -101,12 +156,12 @@ def print_set_summary():
     if len(failed_tests) > 0:
         print (thin_border_line + '\n\n')
     
-def run_test(dir, file):
+def run_test(dir, file, ignore_command=[]):
     global total_tests_completed
     global failed_tests
 
-    tests_completed = 0
-    tests_timedout  = 0
+    test_completed = [False] * len(command)
+    test_timedout  = [False] * len(command)
 
     print(oc.PURPLE, '[' + str(datetime.now()) + ']', oc.ENDC, 'Running Test ' + file[:-2])
 
@@ -114,6 +169,12 @@ def run_test(dir, file):
     csv_out = dir + ',' + file[:-2] + ','
 
     for i in range(len(command)):
+        if i in ignore_command:
+            print(oc.RED, 'Assumed Timeout (' + algo(i) + ')', oc.ENDC)
+            test_timedout[i] = True
+            csv_out = csv_out + make_csv_row('TIMEOUT (assumed)', '', '', i)
+            continue
+
         start_time = time()
         try:
             p = subprocess.Popen(command[i],
@@ -137,14 +198,15 @@ def run_test(dir, file):
         error       = ''
         
         if returncode == 0: # OK
-            tests_completed += 1
+            test_completed[i] = True
         elif returncode == 42: # assert_fail
             error = 'assert fail'
-            tests_completed += 1
+            test_completed[i] = True
         elif returncode == 124: # timeout
+            assert(float(test_time) >= TO)
             print(oc.RED, 'Timeout (' + algo(i) + ')', oc.ENDC)
             csv_out = csv_out + make_csv_row('TIMEOUT', test_time, error, i)
-            tests_timedout += 1
+            test_timedout[i] = True
             continue
         else:
             error_code = str(returncode)
@@ -160,8 +222,8 @@ def run_test(dir, file):
 
         csv_out = csv_out + make_csv_row(trace_count, test_time, error, i)
 
-    total_tests_completed += tests_completed
-    return tests_completed, tests_timedout, csv_out
+    total_tests_completed += test_completed.count(True)
+    return test_completed, test_timedout, csv_out
 
 # main --------------------------------------------
 
@@ -170,6 +232,11 @@ while len(benchdirs) > 0:
     tests_failed    = 0
     tests_timedout  = 0
 
+    if has_configs:
+        last_config = {}
+        [last_config.setdefault(x,[]) for x in range(len(command))] # {0:[], 1:[], 2:[]}
+        last_status_TO = [False] * len(command)
+
     bench_path = benchdirs.pop()
     if bench_path[-1] != '/':
         bench_path += '/'
@@ -177,19 +244,29 @@ while len(benchdirs) > 0:
     benchdirs  += [ f.path for f in os.scandir(bench_path) if f.is_dir() ]
     bench_dir   = bench_path.split('/')[-2]
 
+    if is_ignore(bench_dir):
+        print('Ignoring Test Set ' + bench_dir)
+        continue
+
     print(oc.BLUE, oc.BOLD, 'Entering', bench_path, oc.ENDC)
     
+    if has_configs:
+        configs = sort_by_config(bench_files)
+        bench_files = [ bench_dir + '_conf_' + '-'.join(config) + '.c' for config in configs ]
     for file in bench_files:
-        if is_ignore(file):
-            print('Ignoring Test ' + file[:-2])
-            continue
-
-        completed_tests, to_tests, test_result = run_test(bench_path, file)
+        assumed_TO_configs = []
+        if has_configs:
+            assumed_TO_configs = generate_assumed_TO_configs(get_config(file), last_config, last_status_TO)
+    
+        test_completed, test_timedout, test_result = run_test(bench_path, file, assumed_TO_configs)
         csvfile.write(test_result)
 
-        tests_completed += completed_tests
-        tests_timedout  += to_tests
-        tests_failed    += len(command) - completed_tests - to_tests
+        tests_completed += test_completed.count(True)
+        tests_timedout  += test_timedout.count(True)
+        tests_failed    += len(command) - test_completed.count(True) - test_timedout.count(True)
+
+        if has_configs:
+            record_TOs(test_timedout, get_config(file), last_status_TO, last_config, assumed_TO_configs)
     
     print(oc.BLUE, oc.BOLD, 'Leaving', bench_path, oc.ENDC)
     print_dir_summary(bench_dir, tests_completed, tests_failed, tests_timedout)
