4501f82596880e62d5a97b278c1e90428137fe06
singhsanjana <sanjana.singh2906@gmail.com>
2022-06-03 21:48:19 +0530

    updated benchmarks

---
+++ benchmarks/sv_bench/pthread-atomic-c11/bakery/bakery_conf_5.cc
@@ -0,0 +1,108 @@
+// #include <threads.h>
+#include <pthread.h>
+#include <stdatomic.h>
+#include <assert.h>
+// #include "librace.h"
+// #include "model-assert.h"
+atomic_int c1;
+atomic_int c2;
+
+atomic_int n1;
+atomic_int n2;
+atomic_int _cc_x;
+
+atomic_int barr;
+#define LOOPt0 5
+#define LOOPt1 5
+void* t0(void *)
+{
+  int rn1 = -1;
+  int rn2 = -1;
+  int rc2 = -1;
+  int rx = -1;
+  for(int j=0;j<LOOPt0;j++){
+   atomic_store_explicit(&c1, 1, memory_order_seq_cst);
+    rn2 =atomic_load_explicit(&n2, memory_order_acquire);
+    rn1 = rn2 + 1;
+   atomic_store_explicit(&n1, rn1, memory_order_seq_cst); // _relaxed);
+   atomic_store_explicit(&c1, 0, memory_order_seq_cst);
+    rc2 = atomic_load_explicit(&c2, memory_order_seq_cst);
+    if(!(rc2 == 0))
+      return NULL;
+    rn2 = atomic_load_explicit(&n2, memory_order_seq_cst);
+    if(!(rn2 == 0 || rn1 < rn2))
+      return NULL;
+   atomic_store_explicit(&_cc_x, 2, memory_order_release);
+    rx = atomic_load_explicit(&_cc_x, memory_order_acquire);
+    assert(rx==2);
+   atomic_store_explicit(&n1, 0, memory_order_seq_cst); // _relaxed);
+
+  }
+  return NULL;
+}
+
+
+void* t1(void *)
+{
+  int rn1 = -1;
+  int rn2 = -1;
+  int rc1 = -1;
+  int rx = -1;
+  for(int j=0;j<LOOPt1;j++){
+   atomic_store_explicit(&c2, 1, memory_order_seq_cst);
+    rn1=atomic_load_explicit( &n1, memory_order_seq_cst); // _relaxed);
+    rn2 = rn1 + 1;
+   atomic_store_explicit(&n2, rn2, memory_order_seq_cst); // _relaxed);
+   atomic_store_explicit(&c2, 0, memory_order_seq_cst);
+    rc1 =atomic_load_explicit(&c1, memory_order_seq_cst);
+    if(!(rc1 == 0))
+      return NULL;
+    rn1=atomic_load_explicit(&n1, memory_order_seq_cst);
+    if(!(rn1 == 0 || rn2 <= rn1))
+      return NULL;
+   atomic_store_explicit(&_cc_x, 1, memory_order_release); // _relaxed);
+    rx =atomic_load_explicit(&_cc_x, memory_order_acquire); // _relaxed);
+    assert(rx==1);
+   atomic_store_explicit(&n2, 0, memory_order_seq_cst); // _relaxed);
+  }
+  return NULL;
+}
+
+int *propertyChecking(void* arg)
+{
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+        pthread_t _t1, _t2;
+        atomic_init(&c1, 0);
+        atomic_init(&c2, 0);
+        atomic_init(&n1, 0);
+        atomic_init(&n2, 0);
+        atomic_init(&barr, 1);
+        atomic_init(&_cc_x, 0);
+        pthread_create(&_t1, NULL, t0, NULL );
+        pthread_create(&_t2, NULL, t1, NULL);
+        pthread_join(_t1, 0);
+        pthread_join(_t2, 0);
+      return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ benchmarks/sv_bench/pthread-atomic-c11/lamport/lamport_conf_2-4-4.cc
@@ -0,0 +1,163 @@
+#include <pthread.h>
+#include <stdatomic.h>
+#include <assert.h>
+// #include "model-assert.h"
+
+#define OFLOOP 2 // outer for loop
+#define IFLOOP 6 // inner for loop
+#define WLOOP  6 // while loop
+
+atomic_int x, y;
+atomic_int b1, b2;
+atomic_int _cc_X;
+
+atomic_int __fence_var;
+
+void* t0(void *)
+{
+  int ry1 = -1, ry2 = -1, ry3 = -1, ry4 = -1, rx = -1, rb2 = -1;
+  for(int l1=0;l1<OFLOOP;l1++) {
+    for(int l2=0;l2<IFLOOP;l2++) 
+    {
+        atomic_store_explicit( &b1, 1, memory_order_relaxed); // memory_order_release);
+        atomic_store_explicit( &x, 1, memory_order_relaxed);
+        ry1 = atomic_load_explicit( &y, memory_order_relaxed);;
+        if (ry1 != 0)
+        {
+            atomic_store_explicit( &b1, 0, memory_order_relaxed); // memory_order_release);
+            ry2 = atomic_load_explicit( &y, memory_order_relaxed);
+            int l3 = 0;
+            while (ry2 != 0 && l3<WLOOP)
+            {
+                ry2 = atomic_load_explicit( &y, memory_order_relaxed);;
+                l3++;
+            };
+            if(ry2!=0)
+                return NULL;
+            continue;
+        }
+        atomic_store_explicit( &y, 1, memory_order_seq_cst); 
+        rx = atomic_load_explicit( &x, memory_order_seq_cst);;
+        if (rx != 1)
+        {
+            atomic_store_explicit( &b1, 0, memory_order_seq_cst); // memory_order_release);
+            rb2 = atomic_load_explicit( &b2, memory_order_seq_cst); // memory_order_acquire);
+            int l3 =0 ;
+            while (rb2 >= 1 &&l3<WLOOP)
+            {
+                rb2 = atomic_load_explicit( &b2, memory_order_seq_cst); // memory_order_acquire);
+                l3++;
+            };
+            if(rb2>=1)
+                return NULL;
+            ry3 = atomic_load_explicit( &y, memory_order_seq_cst);;
+            if (ry3 != 1)
+            {
+                ry4 = atomic_load_explicit( &y, memory_order_seq_cst);
+                l3 = 0;
+                while (ry4 != 0 && l3<WLOOP)
+                {
+                    ry4 = atomic_load_explicit( &y, memory_order_seq_cst);;
+                    l3++;
+                };
+                if(ry4!=0)
+                    return NULL;
+                continue;
+            }
+        }
+        break;
+    }
+    atomic_store_explicit( &_cc_X, 2, memory_order_relaxed); // memory_order_release);
+    rx = atomic_load_explicit( &_cc_X, memory_order_relaxed); // memory_order_acquire);;
+    assert(rx == 2);
+    atomic_store_explicit( &y, 0, memory_order_seq_cst); // memory_order_release); 
+    atomic_store_explicit( &b1, 0, memory_order_seq_cst); // memory_order_release);
+  }
+  return NULL;
+}
+
+void* t1(void *)
+{
+  int ry1 = -1, ry2 = -1, ry3 = -1, ry4 = -1, rx = -1, rb1 = -1;
+  
+for(int l1=0;l1<OFLOOP;l1++) {
+    for(int l2=0;l2<IFLOOP;l2++) 
+    {
+        atomic_store_explicit( &b2, 1, memory_order_relaxed); // memory_order_release);
+        atomic_store_explicit( &x, 2, memory_order_relaxed);
+        ry1 = atomic_load_explicit( &y, memory_order_relaxed);;
+        if (ry1 != 0)
+        {
+            atomic_store_explicit( &b2, 0, memory_order_relaxed); // memory_order_release);
+            ry2 = atomic_load_explicit( &y, memory_order_relaxed);
+            int l3 =0;
+            while (ry2 != 0 && l3 <WLOOP)
+            {
+                ry2 = atomic_load_explicit( &y, memory_order_relaxed);
+                l3++;
+            };
+            if(ry2!=0) return NULL;
+            continue;
+        }
+        atomic_store_explicit( &y, 2, memory_order_seq_cst); 
+        rx = atomic_load_explicit( &x, memory_order_seq_cst);
+        if (rx != 2)
+        {
+            atomic_store_explicit( &b2, 0, memory_order_seq_cst); // memory_order_release);
+            rb1 = atomic_load_explicit( &b1, memory_order_seq_cst); // memory_order_acquire);
+            int l3 =0 ;
+            while (rb1 >= 1 && l3<WLOOP)
+            {
+                rb1 = atomic_load_explicit( &b1, memory_order_seq_cst); // memory_order_acquire);
+                l3++;
+            };
+            if(rb1>=1) return NULL;
+            ry3 = atomic_load_explicit( &y, memory_order_seq_cst);;
+            if (ry3 != 2)
+            {
+                ry4 = atomic_load_explicit( &y, memory_order_seq_cst);
+                l3 =0 ;
+                while (ry4 != 0 &&l3<WLOOP)
+                {
+                    ry4 = atomic_load_explicit( &y, memory_order_seq_cst);
+                    l3++;
+                };
+                if(ry4!=0)return NULL;
+                continue;
+            }
+        }
+        break;
+    }
+    atomic_store_explicit( &_cc_X, 1, memory_order_relaxed); // memory_order_release);
+    rx = atomic_load_explicit( &_cc_X, memory_order_relaxed); // memory_order_acquire);;
+    assert(rx >= 1);
+    atomic_store_explicit( &y, 0, memory_order_seq_cst); // memory_order_release); 
+    atomic_store_explicit( &b2, 0, memory_order_seq_cst); // memory_order_release);
+  }
+  return NULL;
+}
+
+
+int *propertyChecking(void* arg)
+{
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+        pthread_t _t1, _t2;
+        atomic_init(&b1, 0);
+        atomic_init(&b2, 0);
+        atomic_init(&x, 0);
+        atomic_init(&y, 0);
+        atomic_init(&__fence_var, 0);
+        atomic_init(&_cc_X, 0);
+        pthread_create(&_t1, NULL, t0, NULL );
+        pthread_create(&_t2, NULL, t1, NULL);
+        pthread_join(_t1, 0);
+        pthread_join(_t2, 0);
+      return 0;
+}
+
+
+
